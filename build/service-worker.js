b21d16d2e937f26d19c4ab69e9675b30178d726e
Valeri <vakrus@ttu.ee>
2019-05-24 13:51:43 +0300

    'init'

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4b974d1986ae69c438160ae76087c2ad.js"
+  "/precache-manifest.6a14434fef86842bae7ef7e4a471d7e8.js"
 );
 
 self.addEventListener('message', (event) => {

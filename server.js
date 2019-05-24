a7bea8b63d7fa874d3b43a6d492c7c21d3e8962a
Valeri <vakrus@ttu.ee>
2019-05-24 13:31:48 +0300

    'init'

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express')
-const app = express()
 const path = require('path')
-const port = process.env.PORT || 8000
-
+const port = process.env.PORT || 8080
+const app = express()
 app.use(express.static(__dirname))
 app.use(express.static(path.join(__dirname, 'build')))
-
+app.get('/ping', (req, res) => {
+  res.send('pong')
+})
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
-
 app.listen(port)
